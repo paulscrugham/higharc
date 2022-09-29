47ab5c5617c0f865a1292912be547fb7b71f19ca
paulscrugham <paulscrugham@gmail.com>
2022-09-29 09:41:37 -0700

    Add logic to visit faces, fix edge chaining

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ class Vertex {
         this.edges = [];  // Edges
     }
 
-    compFN(a, b) {
+    compFN(b, a) {
         // return false if a is to the left of b, true otherwise.
         // return (a[0] - center[0]) * (b[1] - center[1]) - (a[1] - center[1]) * (b[0] - center[0]);
         return (a.to.x - a.from.x) * (b.to.y - b.from.y) - (a.to.y - a.from.y) * (b.to.x - b.from.x);
@@ -30,9 +30,15 @@ class Vertex {
 
         let prev = this.edges[this.edges.length - 1];
         for (let i = 0; i < this.edges.length; i++) {
-            this.edges[i].next = prev;
+            prev.reverse.next = this.edges[i];
             prev = this.edges[i];
+            // this.edges[i].next = prev;
+            // prev = this.edges[i].reverse;
         }
+    }
+
+    toStr() {
+        return "Vertex " + this.index;
     }
 }
 
@@ -42,17 +48,23 @@ class Edge {
         this.to = to;  // Vertex
         this.visited = false;
         this.next = null;  // Edge
-        this.pair = null;  // Edge
+        this.reverse = null;  // Edge
         from.addEdge(this);  // adds an outgoing edge to the from Vertex object
+    }
+
+    toStr() {
+        return "Edge " + this.from.index + " -> " + this.to.index;
     }
 }
 
 function printVerts(v) {
     for (let i = 0; i < v.length; i++) {
-        console.log("Vertex " + i + " | " + "x: " + v[i].x + ", y: " + v[i].y);
+        console.log(v[i].toStr());
         for (let j = 0; j < v[i].edges.length; j++) {
-            console.log("from: [" + v[i].edges[j].from.x + ", " + v[i].edges[j].from.y + "]" + ", " 
-                + "to: [" + v[i].edges[j].to.x + ", " + v[i].edges[j].to.y + "]");
+            console.log("out: " + v[i].edges[j].toStr() + 
+            ", next: " + v[i].edges[j].next.toStr() + 
+            ", next.next : " + v[i].edges[j].next.next.toStr() + 
+            ", next.next.next : " + v[i].edges[j].next.next.next.toStr());
         }
     }
 }
@@ -77,9 +89,9 @@ for (let i = 0; i < g.edges.length; i++) {
     e[2*i] = new Edge(from, to);
     e[2*i+1] = new Edge(to, from);
 
-    // set edge pairs
-    e[2*i].pair = e[2*i+1];
-    e[2*i+1].pair = e[2*i];
+    // set edge reverse
+    e[2*i].reverse = e[2*i+1];
+    e[2*i+1].reverse = e[2*i];
 }
 
 // 3. sort outgoing edges for each Vertex
@@ -87,41 +99,25 @@ for (let i = 0; i < v.length; i++) {
     v[i].sortEdges();
 }
 
-console.log(e);
-console.log(v);
+// console.log(e);
+// console.log(v);
 
 printVerts(v);
 
-const faces = []
+// 4. traverse half edges and build faces
+const faces = [];
+for (let i  = 0; i < e.length; i++) {
+    if (e[i].visited == false) {
+        // console.log("in loop");
+        const face = [];
+        let curr = e[i].next;
+        while (!curr.visited) {
+            face.push(curr);
+            curr.visited = true;
+            curr = curr.next;
+        }
+        faces.push(face);
+    }
+}
 
-
-
-
-// function sortCounterClockwise(verts) {
-//     verts.sort(left);
-// }
-
-// // 1.   Find all neighboring vertices and sort counterclockwise
-// // 1a.  Initialize neighboring vertices list
-// const neighbors = [];
-// for (let i = 0; i < g.vertices.length; i++) {
-//     neighbors.push([]);
-// }
-
-// // 1b. Populate neighbor list
-// for (let i = 0; i < g.edges.length; i++) {
-//     neighbors[g.edges[i][0]].push(g.edges[i][1])
-//     neighbors[g.edges[i][1]].push(g.edges[i][0])
-// }
-// // let center;
-// // 1c. Sort each vertex list by counterclockwise order
-
-// center = g.vertices[0];
-// // console.log(center);
-// neighbors[0].sort((a, b) => (a[0] - center[0]) * (b[1] - center[1]) - (a[1] - center[1]) * (b[0] - center[0]));
-// console.log(neighbors[0]);
-
-// // console.log("Neighbors sorted");
-// // console.log(neighbors);
-
-// construct Vertex objects from 
+console.log(faces);
